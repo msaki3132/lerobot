588f8061e247d36c1bbb8ee1aca2d4fb3787cdd7
masaki rikitoku <msaki3132@rikima.com>
2025-10-16 23:00:57 +0900

    add

---
+++ build_env.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+#curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
+#sh ./Miniconda3-latest-MacOSX-arm64.sh
+# conda init
+
+# fix PATH environmental variables at .zshrc
+#conda create -y -n lerobot python=3.10
+#conda activate lerobot
+
+# 必要なライブラリを再インストール (torchcodec, torch, rerun-sdkなど)
+#conda install pytorch torchvision torchaudio -c pytorch
+#conda install rerun-sdk -c conda-forge
+#conda install -c conda-forge rerun-sdk
+#conda install ffmpeg=7.1.1 -c conda-forge
+
+# brew install bazel
+#brew install ffmpeg pkg-config
+#conda install conda-forge::labmaze
+#pip install -e .
+#pip install -e ".[aloha, pusht]"
+
+#pip install lerobot
+#pip install 'lerobot[aloha,pusht]'
+
+
